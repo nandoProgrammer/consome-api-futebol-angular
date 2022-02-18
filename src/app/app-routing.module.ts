e1a4e8ff15421d0cd6ccac6927d1bc40d1258c0a
Fernando Costa <62177135+nandoProgrammer@users.noreply.github.com>
2022-02-17 22:41:43 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
